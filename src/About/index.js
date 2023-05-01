4cb0a856db84d91270fb1f9068881d916334fce6
ErikH <erik221@protonmail.com>
2023-04-30 19:45:51 -1000

    added text to about and resource .js files

--- src/About/index.js
+++ src/About/index.js
@@ -10,7 +10,35 @@ const AboutComponent = () => {
             <div style={{
                 paddingTop: "100px"
             }}>
-                About.
+                    This dojo is a gathering of Aikido students practicing together and learning
+                    from each other.  By sharing our previous experiences, styles, and techniques, all students enjoy a wider array of knowledge of Aikido.  Philosophically, we don't have instructors; we have students who share.
+
+Dennis Oka, 6th Dan - Chief Instructor
+Dennis Jinnohara, 5th Dan
+Don Kurihara, 5th Dan
+Guy Kim, 4th Dan
+Mel Asano, 3rd Dan
+Jeff Edlund, 3rd Dan
+Kristy Kon, 3rd Dan
+​Alan Arakaki, 2nd Dan - treasurer
+Erik Haines, 2nd Dan - President
+Roy Canon, 1st Dan 
+Kelsey Kila, 1st Dan
+Lawrence Proctor, 1st Dan
+Warren Sugimoto, 1st Dan
+David Jackson, 1st Dan - vice-president
+Ryan Watada, 1st Dan
+
+Mililani Aikido Founder - Takaji Ishida Sensei (1906-1996)
+
+Takaji Ishida Sensei was born in Niigata, Japan, in  October,1906.  He worked for the then Waialua Agricultural  Company in 1923 and retired from the Waialua Sugar Company in 1971. He died on 9 August, 1996, at the age of 89.
+
+During his younger days, Ishida Sensei exhibited great interest in the martial arts.  He practiced sumo with former Hawaii Governor George Ariyoshi’s father.  At age 18, he began training in judo.  In 1944, he overcame tremendous obstacles to acquire permission to reorganize judo classes in Hawaii which were banned during the war years.  About 60 judo students trained under him at the Waialua Hongwanji Mission social hall.  He later expanded judo classes in Kahuku, Wahiawa, Mililani, and Kona.
+
+Then, in the early 1950’s, Ishida Sensei witnessed an Aikido demonstration by Koichi Tohei Sensei and was smitten with Aikido.  He had the honor to have studied with Morihei Ueshiba O-Sensei in Japan and Hawaii, and had faithfully shared O-Sensei’s teaching with his students.  Through his leadership and tenaciousness, he overcame tremendous obstacles  and worked tirelessly to establish and stabilize Aikido in Hawaii.  To this end, Ishida Sensei served as the 2nd Vice President of the Hawaii Aiki Kwai. He personally taught Aikido throughout Honolulu and occasionally on the neighbor islands.  Through the Central Pacific Aikido Association which he established, Ishida Sensei opened Aikido dojos at various locations including Waialua, Wahiawa, Waipahu, and Mililani.  For these and his many other achievements and contributions, Ishida Sensei was awarded his 8th Dan rank in Aikido on 9 January 1994.  
+His selfless devotion to Aikido serves as an inspiration to all those who follow in his footsteps.
+
+ If it were not for him, we would not have had Aikido dojos at Waialua, Wahiawa, Waipahu, or Mililani.  There would not be a Central Pacific Aikido Association, and most importantly, there would not be the hundreds of students who remembered Ishida Sensei fondly.  While many of his students continue to train actively, many more inactive students continue to practice the values, morals, and ethics of his Aikido instruction in their daily lives.  I am fortunate to be among those who had the benefit of his instruction.  I was the last to receive a Shodan promotion from him.  With that promotion came a self-inflicted obligation to help carry on Ishida Sensei’s lifetime work to share Aikido with everyone.  It is astonishing to find how many of Ishida Sensei’s former students have the same self-infliction.  
             </div>
             <FooterComponent/>
         </div>
--- src/Resources/index.js
+++ src/Resources/index.js
@@ -10,7 +10,33 @@ const ResourcesComponent = () => {
             <div style={{
                 paddingTop: "100px"
             }}>
-                resources.
+                Dojo Etiquette
+
+When entering or leaving the dojo,  in the direction of the front of the dojo You should also bow when entering or leaving the mat.
+
+Shoes and slippers are not allowed on the mat.
+
+We are all here to learn and must help each other to do so. Please come to practice ready to learn more and to help others learn. Being polite, cooperative, and respectful is very important to everyone's success.
+
+Bow to your partner after practicing a technique, and to any sensei who helped you.
+
+If you are late to class, proceed to the right side-rear of the room and kneel on the mat until an instructor invites you onto the mat.
+
+If you need to leave the mat during class, ask an instructor for permission.
+
+Please keep your finger and toe nails cut short.
+
+When sensei indicate the start/end or class or a technique, please follow directions quickly and do not keep people waiting. 
+
+Talking should be kept to a minimum during practice and should be related to Aikido.
+
+Please pay dues promptly. Mailing envelopes can be found in the attendance book.
+
+Do not change your clothes or fold your gi on the mat.
+
+If you have any questions, concerns, or ideas please feel free to talk to any of the sensei.
+
+Horseplay and overly aggressive activity is not allowed on the mat for the the safety of ALL students on the mat. 
             </div>
             <FooterComponent/>
         </div>
